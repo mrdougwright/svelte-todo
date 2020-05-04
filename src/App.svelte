cfb8de429a04c29442f51ff23efd8fdb42e77935
Doug Wright <mrdougwright@gmail.com>
2020-05-03 22:04:04 -0700

    todo app in 15 lines of code

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,15 @@
 <script>
+  let todos = []
+	function addTodo() {
+		todos = [...todos, '']
+	}
+	function removeSelf(index) {
+		todos = [...todos.slice(0,index), ...todos.slice(index + 1)]
+	}
 </script>
+{#each todos as todo, index}
+	<input bind:value={todos[index]}>
+	<button on:click={() => removeSelf(index)}>X</button>
+	<br/>
+{/each}
+<button on:click={addTodo}>Add</button>
