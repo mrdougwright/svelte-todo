ec47c7243780ca44c38e43719bcbd0e35e1ac5d5
Doug Wright <mrdougwright@gmail.com>
2020-05-03 22:54:24 -0700

    todo list with local storage

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,27 @@
 <script>
-  let todos = []
+	import {readStorage, updateStorage} from './LocalStorage.svelte';
+
+	let storageKey = "todo-svelte"
+	let todos = readStorage(storageKey)
+
 	$: getStarted = todos.length > 0 ? "Add" : "Start"
 
 	function addTodo() {
 		todos = [...todos, '']
+		updateStorage(storageKey, todos)
+	}
+	function removeTodo(index) {
+		todos = [...todos.slice(0,index), ...todos.slice(index + 1)]
+		updateStorage(storageKey, todos)
 	}
 	function addOnKeyPress(e) {
 		e.key === "Enter" ? addTodo() : null
-	}
-	function removeSelf(index) {
-		todos = [...todos.slice(0,index), ...todos.slice(index + 1)]
 	}
 </script>
 
 {#each todos as todo, index}
 	<input bind:value={todos[index]} on:keypress={addOnKeyPress} autofocus>
-	<button on:click={() => removeSelf(index)}>X</button>
+	<button on:click={() => removeTodo(index)}>X</button>
 	<br/>
 {/each}
 
---
+++ src/LocalStorage.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  export function updateStorage(key, item) {
+    localStorage.setItem(key, item)
+  }
+  export function readStorage(key) {
+    const itemStr = localStorage.getItem(key)
+    const items = itemStr === null ? [] : itemStr.split(',')
+    return items
+  }
+</script>
