4f25f69c144d7dd62f441cbeb07216218cca14f3
Doug Wright <mrdougwright@gmail.com>
2020-05-03 22:17:09 -0700

    more levers, more problems

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,22 @@
 <script>
   let todos = []
+	$: getStarted = todos.length > 0 ? "Add" : "Start"
+
 	function addTodo() {
 		todos = [...todos, '']
+	}
+	function addOnKeyPress(e) {
+		e.key === "Enter" ? addTodo() : null
 	}
 	function removeSelf(index) {
 		todos = [...todos.slice(0,index), ...todos.slice(index + 1)]
 	}
 </script>
+
 {#each todos as todo, index}
-	<input bind:value={todos[index]}>
+	<input bind:value={todos[index]} on:keypress={addOnKeyPress} autofocus>
 	<button on:click={() => removeSelf(index)}>X</button>
 	<br/>
 {/each}
-<button on:click={addTodo}>Add</button>
+
+<button on:click={addTodo}>{getStarted}</button>
